634ead32d684ae9110e5543101e55cf2cd204371
Gary Coady <gary@lyranthe.org>
2016-09-18 00:06:11 +0100

    Cleanup

--- src/main/scala/scalajs_fs2/webapp/Implicits.scala
+++ src/main/scala/scalajs_fs2/webapp/Implicits.scala
@@ -8,34 +8,29 @@ import scala.scalajs.js
 
 object Implicits {
   implicit class ElementSyntax(val element: dom.raw.Element) extends AnyVal {
-    private def makeQueue(eventType: String)(implicit S: Strategy): Stream[Task, Queue[Task, Option[dom.Event]]] = {
-      def clickEventListener(queue: Queue[Task, Option[dom.Event]]) = { evt: dom.Event =>
-        queue.enqueue1(Some(evt)).unsafeRunAsync(_ => ())
-      }
-
-      def createAddAndRemove(queue: Queue[Task, Option[dom.Event]], evtType: String) = {
-        val listenerFn = clickEventListener(queue)
-        (Task.delay(element.addEventListener(evtType, listenerFn)), Task.delay(element.removeEventListener(evtType, listenerFn)))
+    private def makeQueue(eventType: String)(implicit S: Strategy) = {
+      def clickEventListener(queue: Queue[Task, dom.Event]): js.Function1[dom.Event, Unit] = { evt: dom.Event =>
+        queue.enqueue1(evt).unsafeRunAsync(_ => ())
       }
 
       def createQueue = {
         for {
-          queue <- async.unboundedQueue[Task, Option[dom.Event]]
-          listenerFn = clickEventListener(queue): js.Function1[dom.Event, Unit]
+          queue <- async.unboundedQueue[Task, dom.Event]
+          listenerFn = clickEventListener(queue)
           _ <- Task.delay(element.addEventListener(eventType, listenerFn))
         } yield (queue, listenerFn)
       }
 
-      def emitQueue(queue: Queue[Task, Option[dom.Event]], listenerFn: js.Function1[dom.Event, Unit]): Stream[Task, Queue[Task, Option[dom.Event]]] =
+      def emitQueue(queue: Queue[Task, dom.Event], listenerFn: js.Function1[dom.Event, Unit]) =
         Stream.emit(queue)
 
-      def cleanupQueue(queue: Queue[Task, Option[dom.Event]], listenerFn: js.Function1[dom.Event, Unit]): Task[Unit] =
-        queue.enqueue1(None).flatMap(_ => Task.delay(element.removeEventListener(eventType, listenerFn)))
+      def cleanupQueue(queue: Queue[Task, dom.Event], listenerFn: js.Function1[dom.Event, Unit]) =
+        Task.delay(element.removeEventListener(eventType, listenerFn))
 
       Stream.bracket(createQueue)((emitQueue _).tupled, (cleanupQueue _).tupled)
     }
 
     def stream(eventType: String)(implicit S: Strategy): Stream[Task, dom.Event] =
-      makeQueue(eventType).flatMap(_.dequeueAvailable.unNoneTerminate)
+      makeQueue(eventType).flatMap(_.dequeueAvailable)
   }
 }
